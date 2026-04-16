4d984b7e9eea47bf61d1b6ec9db8f37c460d2e9c
Sakshi Patil <sakshipatil61@gmail.com>
2026-04-16 17:24:41 +0530

    feat: check if array is sorted

--- algorithms/linear-search/check_if_array_is_sorted.py
+++ algorithms/linear-search/check_if_array_is_sorted.py
@@ -1,3 +1,14 @@
+"""
+    Check if an array is sorted in ascending order.
+    
+    Approach:    
+    We only compare adjacent elements, because in a sorted array every element must be ≤ next element.
+    
+    Time Complexity : O(n)
+    Space Complexity : O(1) no extra space used
+    """
+
+
 def checkArraySorted(arr):
     for i in range(len(arr) -1):
             if arr[i] > arr[i+1]:
