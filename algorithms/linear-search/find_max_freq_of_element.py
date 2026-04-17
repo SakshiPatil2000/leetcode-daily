e82036c94e388a7ed3f7a3f594397a95a91e16a5
Sakshi Patil <sakshipatil61@gmail.com>
2026-04-17 15:37:16 +0530

    feat: find max freq element

---
+++ algorithms/linear-search/find_max_freq_of_element.py
@@ -0,0 +1,33 @@
+"""
+  Find element with maximum frequency.
+  
+  Approach :
+  For every element, count its frequency using a second loop.
+  Keep track of the element with the highest frequency.  
+  Return the element with maximum count. 
+  
+"""
+
+
+def findMaxFreqElement(arr):
+    n = len(arr)
+    max_count = 0
+    max_element = -1
+    
+    for i in range(n):
+        count = 0
+        
+        for j in range(n):
+            if arr[i] == arr[j]:
+                count +=1
+        
+        #update max element
+        
+        if count > max_count:
+            max_count = count
+            max_element = arr[i]
+            
+    return max_element
+
+arr =[1,4,5,6,7,8,9,3,2,3,4,5,2,2,3,3,45,7,4,5,6,6,6]
+print(findMaxFreqElement(arr))
