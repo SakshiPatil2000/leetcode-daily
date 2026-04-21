cb68ca963e9174328990a1c093f884fdad3e6991
Sakshi Patil <sakshipatil61@gmail.com>
2026-04-21 13:10:20 +0530

    fix: added method to find all indices

--- algorithms/linear-search/find_all_pairs_with_given_sum.py
+++ algorithms/linear-search/find_all_pairs_with_given_sum.py
@@ -28,6 +28,37 @@ target=6
 print(findAllPairsWithGivenSum(arr,target))
 
 
+"""
+  Find All indices with given sum.
+    
+  Time Complexity = O(n)
+  Space Complexity = O(n)
+      
+"""
+
+def findAllIndicesWithGivenSum(arr2,sum):
+    seen ={}
+    pairs=[]
+    
+    for i,num in enumerate(arr2):
+        complement = sum - num
+        if complement in seen :
+            for idx in seen[complement]:
+                pairs.append((idx,i))
+                
+        if num in seen:
+            seen[num].append(i)
+        
+        else:
+            seen[num] =[i]
+        
+             
+    return pairs
+arr2=[2,7,11,15,8,1]
+sum =9
+print(findAllIndicesWithGivenSum(arr2,sum))
+
+
 
 
 
