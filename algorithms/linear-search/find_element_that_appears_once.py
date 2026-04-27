1073e511a57476d91ebd5176f69cfee5b4ce10b7
Sakshi Patil <sakshipatil61@gmail.com>
2026-04-27 17:27:56 +0530

    feat: find element that appears once

--- algorithms/linear-search/find_element_that_appears_once.py
+++ algorithms/linear-search/find_element_that_appears_once.py
@@ -1,3 +1,22 @@
+"""
+  Find element that appears once.
+  
+  Approach:
+  Use Bit Manipulation(XOR)
+  
+  Time Complexity : O(n)
+  Space Complexity : O(1)  
+
+"""
+
 def findElementThatAppearsOnce(arr):
+    result = 0
     
-    return value
+    for num in arr:
+        
+         result ^= num
+         
+    return result
+
+arr = [2,3,5,4,5,3,4]
+print(findElementThatAppearsOnce(arr))
